fc2cfc8e436acdf089b115090b670280085eb909
flier268 <flier268@gmail.com>
2022-06-27 18:57:05 +0800

    Add unit test

---
+++ Fanhuaji-APITests/FanhuajiTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Fanhuaji_APITests
+{
+    [TestFixture]
+    public class FanhuajiTests
+    {
+        [Test]
+        public void Constrocotr_throw_exception_while_user_not_accept_EULA()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<Exception>(() => { new Fanhuaji(false, Fanhuaji.Terms_of_Service); });
+                Assert.Throws<Exception>(() => { new Fanhuaji(false, null); });
+                Assert.Throws<Exception>(() => { new Fanhuaji(true, null); });
+                Assert.DoesNotThrow(() => { new Fanhuaji(true, Fanhuaji.Terms_of_Service); });
+            });
+        }
+
+        [Test]
+        public void CheckConnection_should_pass_while_api_is_useable()
+        {
+            // Act
+            Assert.That(Fanhuaji.CheckConnection(), Is.True);
+        }
+
+        [Test]
+        public void ConvertAsync_should_return_corresponding_value()
+        {
+            // Arrange
+            Fanhuaji? fanhuaji = new(true, Fanhuaji.Terms_of_Service);
+            Assert.Multiple(async () =>
+            {
+                Assert.DoesNotThrowAsync(async () => await fanhuaji.ConvertAsync("繁體轉簡體", Enum_Converter.Simplified));
+                // Act
+                Assert.That((await fanhuaji.ConvertAsync("简体转繁体", Enum_Converter.Traditional)).Data.Text, Is.EqualTo("簡體轉繁體"));
+                Assert.That((await fanhuaji.ConvertAsync("繁體轉簡體", Enum_Converter.Simplified)).Data.Text, Is.EqualTo("繁体转简体"));
+            });
+        }
+    }
+}
---
+++ Fanhuaji-APITests/Models/ModuleTests.cs
@@ -0,0 +1,69 @@
+﻿using Fanhuaji_API.Models;
+
+namespace Fanhuaji_APITests.Models
+{
+    [TestFixture]
+    public class ModuleTests
+    {
+        [Test]
+        public void Name_should_be_defined()
+        {
+            // Arrange
+            Assert.DoesNotThrow(() =>
+            {
+                foreach (var modules in Enum.GetValues<Enum_Modules>())
+                {
+                    Module module = new(modules, false);
+                    _ = module.Name;
+                }
+            });
+        }
+
+        [Test]
+        public void Type_should_be_defined()
+        {
+            // Arrange
+            Assert.DoesNotThrow(() =>
+            {
+                foreach (var modules in Enum.GetValues<Enum_Modules>())
+                {
+                    Module module = new(modules, false);
+                    _ = module.Type;
+                }
+            });
+        }
+
+        [Test]
+        public void Description_should_be_defined()
+        {
+            // Arrange
+            Assert.DoesNotThrow(() =>
+            {
+                foreach (var modules in Enum.GetValues<Enum_Modules>())
+                {
+                    Module module = new(modules, false);
+                    _ = module.Description;
+                }
+            });
+        }
+
+        [Test]
+        public void ModuleName_and_enable_should_be_equal_constructor_arg()
+        {
+            // Arrange
+            foreach (var modules in Enum.GetValues<Enum_Modules>())
+            {
+                Assert.Multiple(() =>
+                {
+                    Module module = new(modules, false);
+                    Assert.That(module.ModuleName, Is.EqualTo(modules));
+                    Assert.That(module.Enable, Is.False);
+
+                    module = new Module(modules, true);
+                    Assert.That(module.ModuleName, Is.EqualTo(modules));
+                    Assert.That(module.Enable, Is.True);
+                });
+            }
+        }
+    }
+}
